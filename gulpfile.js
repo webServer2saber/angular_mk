1510cdb33ed1f16a93964d7505a4c5616acf04b5
canbin <767314091@qq.com>
2017-02-24 23:42:45 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins');
+var open = require('open');
+
+
+var app = {
+    srcPath : 'src/',
+    devPath : 'build/',
+    prdPath : 'dist/'
+};
+
+gulp.task('lib',function(){
+    gulp.src('')
+});
+
+
+
+
+
+
+
+
+
